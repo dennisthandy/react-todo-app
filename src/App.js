010611a89609fabf73a3556f29ec03b95c41f3cf
Dennis Thandy <33387667+dennisthandy@users.noreply.github.com>
2020-08-21 09:23:52 +0700

    add filter

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Todos from "./components/Todos";
 
 export default function App() {
   const [todos, setTodos] = useState([]);
-
   const [todo, setTodo] = useState("");
+  const [filter, setFilter] = useState("");
 
   const addTodo = () => {
     const newTodo = {
@@ -24,14 +24,28 @@ export default function App() {
 
   const clearCompletedTodo = () => {
     setTodos(todos.filter((todo) => !todo.completed));
-    console.log(todos);
+  };
+
+  const checkCompletedTodo = () => todos.some((todo) => todo.completed);
+
+  const filteredTodos = () => {
+    let filtered = [];
+
+    if (filter === "completed" && checkCompletedTodo()) {
+      filtered = todos.filter((todo) => todo.completed);
+    } else if (filter === "ongoing") {
+      filtered = todos.filter((todo) => !todo.completed);
+    } else {
+      filtered = todos;
+    }
+    return filtered;
   };
 
   return (
     <div className="App">
       <h1>Todo App</h1>
       <div className="todo-container">
-        <Todos todos={todos} checkTodo={checkTodo} />
+        <Todos todos={filteredTodos()} checkTodo={checkTodo} />
         <input
           className="todo-input"
           placeholder="Add Todo"
@@ -51,8 +65,37 @@ export default function App() {
           <button className="button-clear" onClick={clearCompletedTodo}>
             Clear Completed Todo
           </button>
+          <button
+            className={`button-filter ${filter === "all" ? "active" : ""}`}
+            onClick={() => setFilter("all")}
+          >
+            Show All Todo
+          </button>
+          <button
+            className={`button-filter ${
+              filter === "completed" && checkCompletedTodo() ? "active" : ""
+            }`}
+            onClick={() => setFilter("completed")}
+          >
+            Show Completed Todo
+          </button>
+          <button
+            className={`button-filter ${filter === "ongoing" ? "active" : ""}`}
+            onClick={() => setFilter("ongoing")}
+          >
+            Show Ongoing Todo
+          </button>
         </div>
       </div>
+      <footer>
+        <p>
+          Made with{" "}
+          <span role="img" aria-label="heart">
+            ❤️
+          </span>{" "}
+          by Dennis Thandy
+        </p>
+      </footer>
     </div>
   );
 }
